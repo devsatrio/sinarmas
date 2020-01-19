e47657e38cd36a694ea29a4d9af5f11636218b67
devasatrio <34015427+devsatrio@users.noreply.github.com>
2020-01-19 12:01:55 +0700

    crud blogq

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class artikel(models.Model):
+    judul = models.CharField(max_length=50)
+    isi = models.TextField(max_length=1000)
+    tanggal = models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    slug = models.SlugField(editable=False,blank=True,null=True)
+
+    def save(self):
+        self.slug = slugify(self.judul)
+        super(artikel, self).save()
+
+    def __str__(self):
+        return self.judul

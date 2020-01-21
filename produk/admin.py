43034c7bdaa5bd2cffb2363f9df9ab7f4cd70681
notbeable <blackwolf2103@outlook.com>
2020-01-21 23:01:29 +0700

    import export

--- produk/admin.py
+++ produk/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import produk
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-class tampilan(admin.ModelAdmin):
+class ProdukResource(resources.ModelResource):
+
+	class Meta:
+		model = produk
+		fields = ('nama_produk','kategori_barang','deskripsi','harga')
+
+class tampilan(ImportExportModelAdmin):
+	resource_class = ProdukResource
+	readonly_fields = ['slug','tanggal']
 	list_display=('nama_produk','harga','deskripsi','kategori_barang','gambar','tanggal')
 	readonly_fields=['tanggal']
 
--- produk/models.py
+++ produk/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 from kategori_barang.models import barang
 
 class produk(models.Model):
@@ -8,6 +9,11 @@ class produk(models.Model):
 	kategori_barang = models.ForeignKey(barang,on_delete=models.SET_NULL,null=True) 
 	gambar = models.ImageField(upload_to='produk_image/',null=True)
 	tanggal = models.DateTimeField(auto_now_add=True,null=True)
+	slug = models.SlugField(editable=False,blank=True,null=True)
+
+	def save(self):
+		self.slug=slugify(self.kategori_barang)
+		super(produk, self).save()
 
 	def __str__(self):
 		return self.nama_produk
